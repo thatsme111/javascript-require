495a70708f54f7e55d1b72e1ced86a58b7d2b315
thatsme <shailesh.nighojkar@gslab.com>
2016-01-11 15:19:46 +0530

    initiali commit

---
+++ javascript_require.js
@@ -0,0 +1,10 @@
+window.require = function(scriptName){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+		  eval(xhttp.responseText);
+		}
+	};
+	xhttp.open("GET", scriptName, true);
+	xhttp.send();
+}
